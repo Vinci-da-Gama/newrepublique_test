373127ec90ce2742779982dc0458626f3a62c522
Vinci-da-Game <11582145@student.uts.edu.au>
2018-06-03 23:31:42 +1000

    grid row3

--- wp-content/themes/newrepublique_slug/asserts/_js/cust.js
+++ wp-content/themes/newrepublique_slug/asserts/_js/cust.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    newrepubliqueCarousel();
+	newrepubliqueCarousel();
+
+    const leftsider2 = $('.left2'),
+    	  rightsider2 = $('.right2');
+    // wobble, tada, bounceInUp
+    const fil = 'fadeInLeftBig',
+        fir = 'fadeInRightBig';
+    const row2pos = '50%',
+    	  row3pos = '65%',
+    	  row4pos = '80%';
+    sideWaypoint(leftsider2, fil, row2pos);
+    sideWaypoint(rightsider2, fir, row2pos);
 });
 
 function newrepubliqueCarousel() {
@@ -13,5 +24,21 @@ function newrepubliqueCarousel() {
         adaptiveHeight: true,
         vertical: true,
         verticalSwiping: true
+    });
+};
+
+function sideWaypoint(content, animateType, pos) {
+    content.waypoint(function(direction) {
+        var classesNeed = 'display-whenwaypoint-ready ' + animateType + ' animated';
+        if (direction === 'down' && !content.hasClass('animated')) {
+            // var relatedElem = $(this);
+            console.log('139 -- content is: ', content);
+            content.addClass(classesNeed);
+        }
+    }, {
+        // offset is A percentage of the viewport's height
+        // It define how long from top you want to trigger the animation
+        // there are 2 units of it , one is % ('50%') another is px(number only).
+        offset: pos
     });
 };
